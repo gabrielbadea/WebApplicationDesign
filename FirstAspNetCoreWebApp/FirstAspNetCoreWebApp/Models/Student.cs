65e0e67ff2479d97cefaf0d4212249fe57f47c2d
gabrielbadea <gabriel.badea@yahoo.com>
2022-02-03 10:25:24 +0200

    Sample Net Core 6 app

---
+++ FirstAspNetCoreWebApp/FirstAspNetCoreWebApp/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstAspNetCoreWebApp.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string? StudentName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
