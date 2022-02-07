06e799ecac2f94879246f5407ca46c9825eabad9
gabrielbadea <gabriel.badea@yahoo.com>
2022-02-07 17:06:57 +0200

    Asp Net Core AJAX Sample Call App

---
+++ AspNetCoreAjaxSampleCallApp/AspNetCoreAjaxSampleCallApp/Models/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCoreAjaxSampleCallApp.Models
+{
+    public class Location
+    {
+        public bool IsNumber { get; set; }
+
+        public string? LocationName { get; set; }
+    }
+}
