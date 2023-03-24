6497de5666eba6ca85c6154042b5498466dcf445
gabrielbadea <gabriel.badea@yahoo.com>
2023-03-24 10:26:57 +0200

    Remove AspNetCoreEntityFrameworkApp

--- AspNetCoreEntityFrameworkApp/AspNetCoreEntityFrameworkApp/AspNetCoreEntityFrameworkApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AspNetCoreEntityFrameworkApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
