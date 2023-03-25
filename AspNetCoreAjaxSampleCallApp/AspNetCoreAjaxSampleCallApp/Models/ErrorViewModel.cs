db2a8ccf884378a426a2c3306c8a251f3759748a
gabrielbadea <gabriel.badea@yahoo.com>
2023-03-25 12:34:04 +0200

    Remove AspNetCoreAjaxSampleCallApp

--- AspNetCoreAjaxSampleCallApp/AspNetCoreAjaxSampleCallApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AspNetCoreAjaxSampleCallApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- AspNetCoreAjaxSampleCallApp/AspNetCoreAjaxSampleCallApp/Models/Location.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AspNetCoreAjaxSampleCallApp.Models
-{
-    public class Location
-    {
-        public bool IsNumber { get; set; }
-
-        public string? LocationName { get; set; }
-    }
-}
