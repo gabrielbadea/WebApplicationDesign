f95a8f9b195c8567b099209c775f6994a906ad0f
gabrielbadea <gabriel.badea@yahoo.com>
2023-03-30 15:12:10 +0300

    Remove AspNetCoreSecurityApp

--- AspNetCoreSecurityApp/AspNetCoreSecurityApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AspNetCoreSecurityApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
