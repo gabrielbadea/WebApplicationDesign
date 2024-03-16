6fd138e61b4e7ca922cc5b51334779bd3b95f563
gabrielbadea <gabriel.badea@yahoo.com>
2024-03-16 17:24:57 +0200

    AspNetCoreEFCoreApp

--- AspNetEntityFrameworkCoreApp/AspNetEntityFrameworkCoreApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AspNetEntityFrameworkCoreApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
