3d39f6f47999c08698d8d4d2f5cb5ab96f3a1b40
gabrielbadea <gabriel.badea@yahoo.com>
2026-02-23 11:45:43 +0200

    Lab 4 update

---
+++ Lab 4. AspNetCoreSampleApp/AspNetCoreSampleApp/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetCoreSampleApp.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string? StudentName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
